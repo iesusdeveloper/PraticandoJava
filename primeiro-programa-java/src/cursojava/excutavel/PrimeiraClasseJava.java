966fb37c5feb894eaa990fed261a6688e0b19789
Iesus Magdiel <Iesus Magdiel@Iesus>
2022-11-04 16:28:14 -0300

    new commit

--- primeiro-programa-java/src/cursojava/excutavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/excutavel/PrimeiraClasseJava.java
@@ -5,6 +5,6 @@ public class PrimeiraClasseJava {
 	/*Main é um método auto executável em java*/
 	public static void main(String[] args) {
 		System.out.println("Meu primeiro programa em javaa " + (1+1));
-		System.out.println("aqui");
+		System.out.println("aqui agora");
 	}
 }
