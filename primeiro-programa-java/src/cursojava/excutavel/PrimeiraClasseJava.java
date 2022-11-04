39ff059919e96cc7ff8038b8a89168664457260e
Iesus Magdiel <Iesus Magdiel@Iesus>
2022-11-04 16:17:10 -0300

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/excutavel/PrimeiraClasseJava.java
@@ -0,0 +1,10 @@
+package cursojava.excutavel;
+
+public class PrimeiraClasseJava {
+	
+	/*Main é um método auto executável em java*/
+	public static void main(String[] args) {
+		System.out.println("Meu primeiro programa em javaa " + (1+1));
+		System.out.println("aqui");
+	}
+}
