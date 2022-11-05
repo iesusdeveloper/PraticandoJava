44bf2b5b9573bdd6cdb9892d1e9f36aa9f1aa384
Iesus Franco <76500125+iesusdeveloper@users.noreply.github.com>
2022-11-05 11:55:33 -0300

    Update PrimeiraClasseJava.java

--- primeiro-project-java/src/cursojavas/execute/PrimeiraClasseJava.java
+++ primeiro-project-java/src/cursojavas/execute/PrimeiraClasseJava.java
@@ -1,10 +1,11 @@
 package cursojavas.execute;
 
 public class PrimeiraClasseJava {
-	/*Main È um mÈtodo auto execut·vel em Java*/
+	/*Main √© um m√©todo auto execut√°vel em Java*/
 	public static void main(String[] args) {
 		System.out.println("Meu primeiro programa em javaa " + (1+1));
 		System.out.println("aqui agora");
+		System.out.println("testando Pull");
 	}
 }
 
