9b7fa26ac95708017bdd9828c1b516dea5a2f68f
Iesus Magdiel <Iesus Magdiel@Iesus>
2022-11-05 10:03:23 -0300

    Tentando comitar novamente

--- primeiro-programa-java/src/cursojava/excutavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/excutavel/PrimeiraClasseJava.java
@@ -2,7 +2,7 @@ package cursojava.excutavel;
 
 public class PrimeiraClasseJava {
 	
-	/*Main é um método auto executável em java*/
+	/*Main é um método auto executável em Java*/
 	public static void main(String[] args) {
 		System.out.println("Meu primeiro programa em javaa " + (1+1));
 		System.out.println("aqui agora");
