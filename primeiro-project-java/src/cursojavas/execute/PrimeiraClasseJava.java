b9e0cd5fb797bb0571e9c90cc9d79604f5b343dc
Iesus Magdiel <Iesus Magdiel@Iesus>
2022-11-05 10:19:34 -0300

    Primeiro Commit em java

---
+++ primeiro-project-java/src/cursojavas/execute/PrimeiraClasseJava.java
@@ -0,0 +1,10 @@
+package cursojavas.execute;
+
+public class PrimeiraClasseJava {
+	/*Main é um método auto executável em Java*/
+	public static void main(String[] args) {
+		System.out.println("Meu primeiro programa em javaa " + (1+1));
+		System.out.println("aqui agora");
+	}
+}
+
